b0f5d6a99700b090effe54a447c8038180f7756c
Nelson Hsu <nelsonhsu1027@gmail.com>
2019-09-13 10:53:40 +0800

    Cordova & Capacitor's plugins can work in Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       <a class="navbar-brand" href="#">Vue_Cor_Ble_Nelson_Test</a>
     </nav>
     <button type="button" class="btn btn-info" @click="call">掃瞄</button>
+    <button type="button" class="btn btn-info" @click="photo">照相</button>
 
     <ul class="list-group" v-for="l in list">
       <li class="list-group-item list-group-item-success">
@@ -12,16 +13,22 @@
         RSSI:{{l.rssi}}
       </li>
     </ul>
+    <br />
+    相片url：{{photo_url}}
     <router-view />
   </div>
 </template>
 
 <script>
+import { Plugins, CameraResultType } from "@capacitor/core";
+const { Camera } = Plugins;
+
 export default {
-  name: 'home',
+  name: "home",
   data() {
     return {
-      list: []
+      list: [],
+      photo_url: ""
     };
   },
   methods: {
@@ -57,9 +64,24 @@ export default {
           console.log("stopScan 失敗");
         }
       );
+    },
+    async photo() {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri
+      });
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      var imageUrl = image.webPath;
+      // Can be set to the src of an image now
+      imageElement.src = imageUrl;
+      this.photo_url = imageUrl;
     }
   }
-}
+};
 </script>
 
 
